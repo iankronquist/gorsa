7bc14212f6703d6d4f9ef8abf2b004fe6bf2e2e8
Ian Kronquist <iankronquist@teleport.com>
2015-02-04 16:03:38 -0800

    Add a cipher interface which CBC will implement

---
+++ cipher/cipher.go
@@ -0,0 +1,15 @@
+package cipher
+
+import (
+	"math/big"
+	"../key"
+)
+
+type Cipher interface {
+	Encrypt(message *big.Int, cipherKey *key.Key) *big.Int
+	Decrypt(message *big.Int, cipherKey *key.Key) *big.Int
+	Encrypt_Bytes(message *[]byte, cipherKey *key.Key) *[]byte
+	Decrypt_Bytes(cipherText *[]byte, cipherKey *key.Key) *[]byte
+	KeyGen() *key.Key
+}
+
