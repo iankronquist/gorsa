c3e3c304f93fcbc7db6326867692f76c46eb7301
Ian Kronquist <iankronquist@teleport.com>
2015-02-04 16:17:10 -0800

    Make RSA implement the cipher interface

--- rsa/rsa.go
+++ rsa/rsa.go
@@ -5,15 +5,33 @@ import (
 	"../key/rsakey"
 )
 
-func RSA (message, priv_key, modulus *big.Int) *big.Int {
+type RSACipher struct {}
+
+func (cipher *RSACipher) RSA (message, priv_key, modulus *big.Int) *big.Int {
 	return message.Exp(message, priv_key, modulus)
 }
 
-func RSA_Bytes (message [] byte, key, modulus *big.Int) [] byte {
+func (cipher *RSACipher) RSA_Bytes (message [] byte, key, modulus *big.Int) [] byte {
 	message_num := big.Int{}
 	message_num.SetBytes(message)
 	cipher_text_num := RSA(&message_num, key, modulus)
 	return cipher_text_num.Bytes()
 }
 
-func Encrypt
+func (cipher *RSACipher) Encrypt_Bytes (message [] byte, key,
+	modulus *big.Int) [] byte {
+	return RSA_Bytes(message, key, modulus)
+}
+
+func (cipher *RSACipher) Decrypt_Bytes (message [] byte, key,
+	modulus *big.Int) [] byte {
+	return RSA_Bytes(message, key, modulus)
+}
+
+func (cipher *RSACipher) Encrypt (message, pub_key, modulus *big.Int) *big.Int {
+	RSA (message, pub_key, modulus)
+}
+
+func (cipher *RSACipher) Decrypt (message, priv_key, modulus *big.Int) *big.Int {
+	RSA (message, priv_key, modulus)
+}
